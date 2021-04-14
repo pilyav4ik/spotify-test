bbf33f07bd2344925d1aadd469e15d0cdf1c3719
pilyav4ik <ipiliavskyi@gmail.com>
2021-04-14 13:18:46 +0200

    #SC-1 Scenario: User authorization with token run with token

--- src/test/java/api/token/Token.java
+++ src/test/java/api/token/Token.java
@@ -3,7 +3,7 @@ package api.token;
 public class Token {
 
     public String getToken() {
-        String token = "";
+        String token = "BQB3nKOhTTApZh2-iHsUwmqdgw7ctgC35i2Gu7Xfs6Wmb1q3VL0Q0jMd4pCGFg1NJm20dtwywyGVE0lVk4a81fWuSP5HXqLePQ0oqNQEiXNDmtFGQDbG7DKoY1XInx32Y8ZC9rMJPI4jDJltcZc0Vq5gJ8VV3IZmU1IhixU8wF0oiDY6NqLK93J_xxuFrcv238LtiCuEEgw0HD5HIn4tfGGfoZCtmgO1hQIAF31__b7oSEfXKvw8qUxjC7Hwdootjx3CekDRXFZ2zV3R-9tGi7y__2p-0iYeAqc6eF9y";
         return token;
     }
 }
