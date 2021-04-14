9d74b712debd6257021b5f928b2cf7aa2d473fe8
pilyav4ik <ipiliavskyi@gmail.com>
2021-04-14 13:21:52 +0200

    #SC-1 change token

--- src/test/java/api/token/Token.java
+++ src/test/java/api/token/Token.java
@@ -3,7 +3,7 @@ package api.token;
 public class Token {
 
     public String getToken() {
-        String token = "BQB3nKOhTTApZh2-iHsUwmqdgw7ctgC35i2Gu7Xfs6Wmb1q3VL0Q0jMd4pCGFg1NJm20dtwywyGVE0lVk4a81fWuSP5HXqLePQ0oqNQEiXNDmtFGQDbG7DKoY1XInx32Y8ZC9rMJPI4jDJltcZc0Vq5gJ8VV3IZmU1IhixU8wF0oiDY6NqLK93J_xxuFrcv238LtiCuEEgw0HD5HIn4tfGGfoZCtmgO1hQIAF31__b7oSEfXKvw8qUxjC7Hwdootjx3CekDRXFZ2zV3R-9tGi7y__2p-0iYeAqc6eF9y";
+        String token = "BQCdILUU5gClfxsbinHAqbc7AR37oBSHedXcn8EUaO7VomTid9k66iwO60SQpjjDqBprKVP31GmrhoI-oLh0mpReBObz_XZ8RztUtU7nyf5LVMJFX-CBu9pNHueCunnMUfNvi03xNg1tbKkipA5V2EbWOAIWP1PeKIlTQD5I2zTp_vwjR_FzEJW_o9uayCbe_TjYs0lVNH9Sy_LN1RS7ygRvG9BhCcEcpqluV2Ihe2KcudaGSweSmzIFUTb7kElvoBfOq7phZ0L5lvMXW06byJSXVC7x1h0GNVSH0u7O";
         return token;
     }
 }
